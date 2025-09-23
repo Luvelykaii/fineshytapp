a5ed27645bf7d72d3eaff102a93fc71e47c2f026
Kai <luvelykai@proton.me>
2025-09-23 10:21:50 -1000

    Changed background color on page two 9/23

--- src/app/layer02/page.tsx
+++ src/app/layer02/page.tsx
@@ -1,6 +1,6 @@
 export default function Layer2Page() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#ff8fab] to-[#fec5e5] text-white">
+		<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#de5d83] to-[#ffb7ce] text-white">
 			<div className="flex flex-wrap gap-10">
       
 
